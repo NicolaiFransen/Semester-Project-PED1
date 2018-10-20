163c9c98c076ee174946f82688517c479f73f79b
Murgui <nmurgui@gmail.com>
2018-10-21 00:59:23 +0200

    Updated Script for including boost converter
    Right now the model is created but there's some kind of error and the DC gain is not correct. In any case when the error is solved the scriptting will be ready

--- Calculation/Small signal model/Small_signal_model_creation.m
+++ Calculation/Small signal model/Small_signal_model_creation.m
@@ -1,37 +1,34 @@
 %Script for deriving transfer functions of the system
 %19_10_18
-clearvars
-%%Variable Init
-Ron = 1;
 
-%%Inverse matrix calculation with symbolic math - Not Used-
+%% Inverse matrix calculation with symbolic math - Not Used-
 syms Ron D2 Rout
 
 A = ([Ron*D2 2*D2-1; 1-2*D2 (2*D2-1)/Rout]);
 A_inverse = inv(A);
 
-
-%%Line to output transfer function derivation
+%% Buck Converter - Line to output transfer function derivation
 %This snippet has as an input the system's equations: inductor voltage
 %and capacitor current and transforms it to the transfer function of line
 %to output. The matlab functions used are described in: 
 %https://se.mathworks.com/help/symbolic/formula-manipulation-and-simplification.html
-    
+    clearvars
+    clc
     %symbolic math variable declaration
-    syms C s v D1 D2 i Rout Vg L Ron
+    syms C s v D1 D2 il Rout Vg L Ron
     
     %inductor voltage equation (IVE)
-    ind_v_eqn = L * s * i == Ron * D2 * i + (2*D2 - 1) * v + D1 * Vg;
+    ind_v_eqn = L * s * il == Ron * (3*D2 - 2) * il + (2*D2 - 1) * v + D1 * Vg;
 
     %isolate current in IVE
-    i_l = isolate(ind_v_eqn, i);
+    i_l = isolate(ind_v_eqn, il);
 
     %Capacitor current equation (CCE)
-    cap_i_eqn = C * s * v == (1 - 2*D2) * i + (2*D2 - 1)/Rout * v + D1 * Vg;
+    cap_i_eqn = C * s * v == (1 - 2*D2) * il + (2*D2 - 1)/Rout * v; %+ D1 * Vg; comentado tras cambio 1
 
     %Substitute current equation in CCE
     cap_i_eqn_i_substituted = subs(cap_i_eqn, lhs(i_l), rhs(i_l));
-
+    
     %D2 = 1 - D1
     duty_relation = D2 == 1 - D1;
     system_tf = subs(cap_i_eqn_i_substituted, lhs(duty_relation), rhs(duty_relation));
@@ -39,23 +36,83 @@ A_inverse = inv(A);
     %isolate v
     v_tf = isolate(system_tf, v);
 
-    %Divide by Vg.
+    %Divide by Vg
     Line2Out_tf = simplifyFraction(v_tf / Vg);
 
     %Rearrange with factors of s
     Line2Out_tf_factors_s = collect(Line2Out_tf,s);
     pretty(Line2Out_tf_factors_s)
     
+    %%Removing Ron from the expression
+%     Line2out_tf_without_Ron = subs(Line2Out_tf_factors_s, Ron, 0)
+%     pretty(collect(Line2out_tf_without_Ron,s))
+
+    
     %Substitute constant values in function
-    Line2out_real_values = variable_init(Line2Out_tf_factors_s);
+    Line2out_real_values = variable_value_assign(Line2Out_tf_factors_s);
     
     pretty(Line2out_real_values)
 
     %Translation from symbolic transfer function to MATlab transfer
     %function. External library needed.
     G = sym2tf(rhs(Line2out_real_values))
+%     G_book = tf([0.6], [1, 1e-3, 1])
+%     bode(G_book)
+    bode(G)
+
     
-    function tf = variable_init(sym_tf)
+%% Boost Converter - Line to output transfer function derivation
+%This snippet has as an input the system's equations: inductor voltage
+%and capacitor current and transforms it to the transfer function of line
+%to output. The matlab functions used are described in: 
+%https://se.mathworks.com/help/symbolic/formula-manipulation-and-simplification.html
+    clearvars
+    clc
+    %symbolic math variable declaration
+    syms C s v D1 D2 il Rout Vg L Ron
+    
+    %inductor voltage equation (IVE)
+    ind_v_eqn = L * s * il == D2 * v + (1 - 2*D2) * Vg;
+
+    %isolate current in IVE
+    i_l = isolate(ind_v_eqn, il);
+
+    %Capacitor current equation (CCE)
+    cap_i_eqn = C * s * v == D2 * il + (D1 - D2/Rout) * v;
+
+    %Substitute current equation in CCE
+    cap_i_eqn_i_substituted = subs(cap_i_eqn, lhs(i_l), rhs(i_l));
+    
+    %D2 = 1 - D1
+    duty_relation = D2 == 1 - D1;
+    system_tf = subs(cap_i_eqn_i_substituted, lhs(duty_relation), rhs(duty_relation));
+    
+    %isolate v
+    v_tf = isolate(system_tf, v);
+    pretty(v_tf)
+
+    %Divide by Vg
+    Line2Out_tf = simplifyFraction(v_tf / Vg);
+    pretty(Line2Out_tf)
+    
+    %Rearrange with factors of s
+    Line2Out_tf_factors_s = collect(Line2Out_tf,s);
+    pretty(Line2Out_tf_factors_s)
+  
+    %Substitute constant values in function
+    Line2out_real_values = variable_value_assign(Line2Out_tf_factors_s);
+    
+    pretty(Line2out_real_values)
+
+    %Translation from symbolic transfer function to MATlab transfer
+    %function. External library needed.
+    G = sym2tf(rhs(Line2out_real_values))
+
+    bode(G)
+    
+    
+    
+    function tf = variable_value_assign(sym_tf)
         %MATlabs seems to create variables as local so redefinition is
         %needed
         syms D1 C L Ron Rout
@@ -66,8 +123,8 @@ A_inverse = inv(A);
         Vout_rated = Vin_rated * duty_cycle_1;
         C_value = C == 100E-6;
         L_value = L == 100E-6;
-        Rout_value = Rout == Vout_rated^2 / P_rated;
-        Ron_value = Ron == 35E-3;
+        Rout_value = Rout == 0.1; %Vout_rated^2 / P_rated
+        Ron_value = Ron == 0;
         %Substitution of the values in tf
         tf_D = subs(sym_tf, lhs(D1_value), rhs(D1_value));
         tf_C = subs(tf_D, lhs(C_value), rhs(C_value));
@@ -76,3 +133,4 @@ A_inverse = inv(A);
         tf_Ron = subs(tf_Rout, lhs(Ron_value), rhs(Ron_value));
         tf = tf_Ron;
     end
+
