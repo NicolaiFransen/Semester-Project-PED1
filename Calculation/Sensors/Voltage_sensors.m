d2dd82524107d788aab1daf51f1e1139e0f2b8aa
NicolaiFransen <fransendk@gmail.com>
2018-10-28 18:45:09 +0100

    Voltage sensor simulation

--- Calculation/Sensors/Voltage_sensors.m
+++ Calculation/Sensors/Voltage_sensors.m
@@ -3,7 +3,7 @@ clear all
 close all
 clc
 
-%% Input sensor
+%% Input voltage sensor
 % Voltage divider
 Vin_max = 39.9;
 Vin_min = 10;
@@ -26,6 +26,20 @@ V_plus = V_0 + 1;
 V_min = V_0 - 1;
 V_out = 5;
 
-R1 = 10e3;
+R3 = 10e3;                      % R4=R3
 
-R3 = (V_out*R1)/(V_plus-V_min)
+R5 = (V_out*R1)/(V_plus-V_min)  % R6=R5
+
+%% Output voltage sensor
+% Voltage divider
+Vout_max = 90;
+Vout_min = 24;
+
+V_sens = 2;
+I_sens = 1e-3;
+
+R7 = (Vout_max-V_sens)/I_sens
+
+syms R
+eqn = V_sens == Vout_max * (R/(R+R7));
+R8 = solve(eqn, R)
