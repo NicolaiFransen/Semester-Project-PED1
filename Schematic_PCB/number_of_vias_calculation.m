b8b9113329f27c68c7f6f7d73f20ff1d17b584a2
Murgui <nmurgui@gmail.com>
2018-11-11 19:26:25 +0100

    fixed calculation errors

--- Schematic_PCB/number_of_vias_calculation.m
+++ Schematic_PCB/number_of_vias_calculation.m
@@ -1,6 +1,8 @@
-hole_diameter = 1 %mm
+hole_diameter = 0.5 %mm
 trace_width = 10 %mm
 safety_margin = 1.30 %30 percent of safety margin
+copper_thickness = 0.0347 * 2 %% mm/oz * oz, units in mm for 2 oz copper
+trace_area = trace_width * copper_thickness
 
-number_of_vias_needed = (trace_width / ((hole_diameter / 2)^2 * pi)) * safety_margin
+number_of_vias_needed = (trace_area / ((hole_diameter / 2)^2 * pi)) * safety_margin
 number_of_vias_needed
