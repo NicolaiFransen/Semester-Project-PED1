5bf89a8242907a0ef5600ed4a0a49c1519093eaf
Murgui <nmurgui@gmail.com>
2018-11-11 15:16:17 +0100

    Added necessary trace width for 15 A, 5ºC, copper thickness 2 oz

--- Schematic_PCB/number_of_vias_calculation.m
+++ Schematic_PCB/number_of_vias_calculation.m
@@ -1,5 +1,5 @@
 hole_diameter = 1 %mm
-trace_width = 5 %mm
+trace_width = 10 %mm
 safety_margin = 1.30 %30 percent of safety margin
 
 number_of_vias_needed = (trace_width / ((hole_diameter / 2)^2 * pi)) * safety_margin
