3ec8ffad7358b239da51293db3fc47678cc6cda1
Murgui <nmurgui@gmail.com>
2018-11-11 15:06:55 +0100

    Script for calculating the number of vias

---
+++ Schematic_PCB/number_of_vias_calculation.m
@@ -0,0 +1,6 @@
+hole_diameter = 1 %mm
+trace_width = 5 %mm
+safety_margin = 1.30 %30 percent of safety margin
+
+number_of_vias_needed = (trace_width / ((hole_diameter / 2)^2 * pi)) * safety_margin
+number_of_vias_needed
