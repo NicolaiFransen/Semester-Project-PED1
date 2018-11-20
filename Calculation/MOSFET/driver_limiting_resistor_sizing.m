9e8ef2fe9c2ce32cf16cad3388dadbe32041a32a
Murgui <nmurgui@gmail.com>
2018-11-21 00:51:07 +0100

    Driver P1 in progress and MOSFET minor corrections.

--- Calculation/MOSFET/driver_limiting_resistor_sizing.m
+++ Calculation/MOSFET/driver_limiting_resistor_sizing.m
@@ -8,8 +8,8 @@ close all
 V_driver = 12
 
 %rise time of MOSFET, fall time is smaller, then this is the most
-%demanding constraint
-tsw_MOSFET = 15e-9
+%demanding constraint. Obtained from simulation.
+% tsw_MOSFET = 15e-9
 
 %datasheet values
 R_driver = 0.4
@@ -20,19 +20,22 @@ C_gate = 1.815e-9
 V_gs_th = 4.7
 
 %sweep analysis
-R_limiting = 10:5:100;
+R_limiting = 5:5:100;
 
 R = R_driver + R_gate_MOSFET + R_limiting;
 
 %Capacitor charge in a RC circuit
 tsw_RC = -R * C_gate * log(-(V_gs_th/V_driver -1));
 
-%Peak power
+%Peak power dissipation in limiting resistor
 I_pk = V_driver ./ R;
-P_pk = I_pk.^2 .* R;
+P_pk_R_limiting = I_pk.^2 .* R_limiting;
+P_pk_R_MOSFET = I_pk.^2 .* R_gate_MOSFET;
 
 %total time adding MOSFET rise time and RC charge
-tsw_total = tsw_MOSFET + tsw_RC;
+% DEPRECATED, mosfet time not considered. tsw_total will be a delay
+% in the system from the driver input to the actual mosfet switching
+tsw_total = 0 + tsw_RC;
 
 figure(1)
 plot(R_limiting, tsw_total)
@@ -41,8 +44,10 @@ legend('switching time against driver resistor')
 figure(2)
 plot(R_limiting, I_pk)
 hold on
-plot(R_limiting, P_pk)
-legend('I_pk', 'P_pk')
+plot(R_limiting, P_pk_R_limiting)
+hold on 
+plot(R_limiting, P_pk_R_MOSFET)
+legend('I_pk', 'P_pk_R_limiting', 'P-pk-R-MOSFET)')
 
 
 
