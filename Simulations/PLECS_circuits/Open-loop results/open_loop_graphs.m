436c65e9580b993814ec3bda944014e3ea6d8110
NicolaiFransen <fransendk@gmail.com>
2018-12-12 14:39:52 +0100

    Open-loop appendix

--- Simulations/PLECS_circuits/Open-loop results/open_loop_graphs.m
+++ Simulations/PLECS_circuits/Open-loop results/open_loop_graphs.m
@@ -37,14 +37,14 @@ xlim([0.3778, 0.3779]);
 ylim([89, 91]);
 title('Output voltage ripple - Boost (300W)')
 xlabel('Time[s]');
-ylabel('Current[A], Voltage[V]');
+ylabel('Voltage[V]');
 legend('V_{out}');
 
 % Ripples
 Vmax = 90.22;
 Vmin = 89.77;
 
-deltaVout = (Vmax-Vmin)/mean(Vout) * 100 
+deltaVout = (Vmax-Vmin)/((Vmax+Vmin)/2) * 100 
 
 %% Open loop simulation - coil ripple test
 data = csvread('Coil_ripple_test.csv');
@@ -60,18 +60,13 @@ figure(3)
 plot(t, IL, 'r', 'Linewidth', 1.5)
 xlim([0.3777, 0.3778]);
 ylim([2.8, 3.8]);
-title('Inductor ripple')
+title('Inductor current ripple')
 xlabel('Time[s]');
 ylabel('Current[A]');
 legend('I_L');
 
 % Ripples
-Vmax = 90.22;
-Vmin = 89.77;
-
-deltaVout = (Vmax-Vmin)/mean(Vout) * 100 
-
 Imax = 3.465;
 Imin = 3.135;
 
-deltaIL = (Imax-Imin)/mean(IL) * 100
+deltaIL = (Imax-Imin)/((Imax+Imin)/2) * 100
