04814b5dae3cc3e1448b58f4879bd240e70e448e
NicolaiFransen <fransendk@gmail.com>
2018-12-17 14:42:40 +0100

    Nomenclature and open loop update

--- Simulations/PLECS_circuits/Open-loop results/open_loop_graphs.m
+++ Simulations/PLECS_circuits/Open-loop results/open_loop_graphs.m
@@ -72,25 +72,25 @@ Imin = 3.135;
 deltaIL = (Imax-Imin)/((Imax+Imin)/2) * 100
 
 %% Open loop simulation results - input voltage ripple
-data = csvread('Input_ripple_exp.csv');
+data = csvread('Input_ripple.csv');
 
 t = data(:,1);
-Vout = data(:, 2);
+Vout = data(:, 3);
 
 
 figure(2)
 plot(t, Vout, 'b', 'Linewidth', 1.5)
 hold on
 %plot(t, IL, 'r', 'Linewidth', 1.5)
-xlim([-0.05, 0.1]);
-ylim([-0.01, 0.01]);
+xlim([0.2, 0.5]);
+ylim([0, 40]);
 title('Input voltage ripple')
 xlabel('Time[s]');
 ylabel('Voltage[V]');
 legend('V_{In}');
 
-Vmax = 36.94;
-Vmin = 36.91;
+Vmax = 36.9419;
+Vmin = 36.905;
 Vmean = (Vmax+Vmin)/2;
 
 deltaVin = (Vmax-Vmin)/Vmean *100
